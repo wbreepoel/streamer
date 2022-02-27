0f375c5193fc45858662b0dfa3ea9e8e96f17266
wbreepoel <wbreepoel@outlook.com>
2022-02-27 21:27:05 +0100

    Initial

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStreams } from "../../actions";
+import StreamForm from "./StreamForm";
+
+class StreamCreate extends React.Component {
+
+    onSubmit = formValues => {
+        this.props.createStreams(formValues);
+    }
+
+    render () {
+        return (
+            <div>
+                <h3>Create a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit}/>
+            </div>
+        )
+    }
+} 
+
+
+export default connect(null, {createStreams})(StreamCreate)
+       
---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,39 @@
+import React from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    onSubmit = (formValues) => {
+        console.log(formValues)
+    }
+
+    render () {
+        console.log(this.props.stream)
+        if (!this.props.stream) {
+            return (
+                <div>Loading...</div>
+            )
+        }
+        
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm initialValues={_.pick(this.props.stream,"title","description")} onSubmit={this.onSubmit}/>
+            </div>
+        )
+    }
+    
+}
+
+const mapStateToProps = (state, ownProps) => {
+    console.log("ownprops",ownProps);
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, {fetchStream, editStream} )(StreamEdit)
