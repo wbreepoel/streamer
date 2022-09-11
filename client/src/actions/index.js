6e5ade7302e54615106348f8f4126c2ba3fed304
wbreepoel <wbreepoel@outlook.com>
2022-09-11 21:22:25 +0200

    Updated delete and cancel function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -42,11 +42,12 @@ export const fetchStream = (id) => async dispatch => {
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`,formValues)
+    const response = await streams.patch(`/streams/${id}`,formValues)
     dispatch({
         type:"EDIT_STREAM",
         payload: response.data
     })
+    history.push("/")
 }
 
 export const deleteStream = (id) => async dispatch => {
@@ -55,4 +56,5 @@ export const deleteStream = (id) => async dispatch => {
         type:"DELETE_STREAM",
         payload:id
     })
+    history.push('/')
 }
